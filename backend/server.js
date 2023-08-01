8480a0e4fe74906e7aff8722e23e713b8458bf1c
nainoktv <nainoaktvillegas@gmail.com>
2023-07-31 23:50:47 -0700

    creating first express API for testing

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Your API works!');
+})
+
 app.listen(8000, console.log('Server is running on PORT 8000'));
