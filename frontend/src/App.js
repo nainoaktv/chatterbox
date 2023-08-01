624a946772e30e0bd6c8d675272b099e13b0b83d
nainoktv <nainoaktvillegas@gmail.com>
2023-08-01 15:12:16 -0700

    install chakra-ui and test button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { Button } from '@chakra-ui/react'
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Button colorScheme='blue'>Button</Button>
     </div>
   );
 }
