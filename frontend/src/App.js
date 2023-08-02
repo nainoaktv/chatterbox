b08095a0ce7c65bd1ea19ed2568a1209f7d3fe78
nainoktv <nainoaktvillegas@gmail.com>
2023-08-01 21:25:17 -0700

    create chats route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage";
+import Chatpage from "./Pages/Chatpage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/chats" element={<Chatpage />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/Pages/Chatpage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chatpage = () => {
+  return <div>This is the chats route!</div>;
+};
+
+export default Chatpage;
