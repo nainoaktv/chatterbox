a1289f007d410280bfe55d68e45c5af0533a8870
nainoktv <nainoaktvillegas@gmail.com>
2023-08-03 01:12:14 -0700

    initial commit login page

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Container, Box, Text } from "@chakra-ui/react";
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>home</h1>
-    </div>
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="1g"
+        borderWidth="1px"
+        borderColor={"#45A29E"}
+      >
+        <Text color={"#C5C6C7"}>ChatterBox</Text>
+      </Box>
+      <Box></Box>
+    </Container>
   );
 };
 
