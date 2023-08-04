52582f985579d31ae67001dfefe2f6260953ef46
nainoktv <nainoaktvillegas@gmail.com>
2023-08-04 02:06:40 -0700

    start login and signup tabs. TODO style tab text color

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,29 +1,60 @@
 import React from "react";
-import { Container, Box, Text } from "@chakra-ui/react";
+import {
+  Container,
+  Box,
+  Text,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Tab,
+} from "@chakra-ui/react";
 
 const Homepage = () => {
   return (
     <Container maxW="xl" centerContent>
       <Box
-        d="flex"
-        justifyContent="center"
+        d={"flex"}
+        justifyContent={"center"}
         p={3}
-        w="100%"
-        m="40px 0 15px 0"
-        borderRadius="lg"
-        borderWidth="1px"
+        w={"100%"}
+        m={"40px 0 15px 0"}
+        borderRadius={"lg"}
+        borderWidth={"1px"}
         borderColor={"#45A29E"}
       >
         <Text
           color={"#C5C6C7"}
           fontFamily={"Work sans"}
-          fontSize="4xl"
+          fontSize={"4xl"}
           textAlign={"center"}
         >
           ChatterBox
         </Text>
       </Box>
-      <Box></Box>
+      <Box
+        w={"100%"}
+        p={"4"}
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+        borderColor={"#45A29E"}
+      >
+        {/* @TODO: Find information on how to change tabs color theme */}
+        <Tabs isFitted variant="enclosed" borderColor={"#66FCF1"}>
+          <TabList mb="1em">
+            <Tab>LOGIN</Tab>
+            <Tab>SIGN-UP</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>WELCOME BACK! 🚀</p>
+            </TabPanel>
+            <TabPanel>
+              <p>SUP NEW CHATTER 😎</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
     </Container>
   );
 };
