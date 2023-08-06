9f12a22dbacc8214a273b7792a1d4f1ff8c695f2
nainoktv <nainoaktvillegas@gmail.com>
2023-08-06 00:51:58 -0700

    setup useState for all form inputs

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,7 +1,24 @@
-import React from "react";
+import { FormControl, FormLabel, Input, VStack } from "@chakra-ui/react";
+import React, { useState } from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [pic, setPic] = useState();
+
+  return (
+    <VStack spacing={"5px"}>
+      <FormControl id="first-name" isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input
+          placeholder="Enter your name..."
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+    </VStack>
+  );
 };
 
 export default Signup;
