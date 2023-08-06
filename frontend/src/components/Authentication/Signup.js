7b3b3f8d414b4a76b1e418d5c0a6448b3797fd8d
nainoktv <nainoaktvillegas@gmail.com>
2023-08-06 01:15:27 -0700

    add hide and show button for passwords and change focused border color

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,21 +1,79 @@
-import { FormControl, FormLabel, Input, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 
+const primaryColor = "#66FCF1";
+
 const Signup = () => {
+  const [show, setShow] = useState(false);
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [confirmPassword, setConfirmPassword] = useState();
   const [pic, setPic] = useState();
 
+  const handleClick = () => setShow(!show);
+
   return (
     <VStack spacing={"5px"}>
       <FormControl id="first-name" isRequired>
         <FormLabel>Name</FormLabel>
         <Input
+          focusBorderColor={primaryColor}
           placeholder="Enter your name..."
           onChange={(e) => setName(e.target.value)}
         />
+      </FormControl>
+
+      <FormControl id="email" isRequired>
+        <FormLabel>E-Mail</FormLabel>
+        <Input
+          focusBorderColor={primaryColor}
+          type="email"
+          placeholder="chatterbox@gmail.com"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            focusBorderColor={primaryColor}
+            type={show ? "text" : "password"}
+            placeholder="Enter your password..."
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width={"4.5rem"}>
+            <Button h={"1.5rem"} size={"sm"} onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
+      <FormControl id="password" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup>
+          <Input
+            focusBorderColor={primaryColor}
+            type={show ? "text" : "password"}
+            placeholder="Confirm Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width={"4.5rem"}>
+            <Button h={"1.5rem"} size={"sm"} onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
       </FormControl>
     </VStack>
   );
