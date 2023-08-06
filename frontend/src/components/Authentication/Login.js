25cae54d4f23eac6113cacfa5f81fc886b146dcf
nainoktv <nainoaktvillegas@gmail.com>
2023-08-06 01:39:10 -0700

    add login input, buttons, and guest user credentials

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from "@chakra-ui/react";
+
+const primaryColor = "#66FCF1";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [show, setShow] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleClick = () => setShow(!show);
+
+  const submitHandler = () => {};
+
+  return (
+    <VStack spacing={"5px"}>
+      <FormControl id="email" isRequired>
+        <FormLabel>E-Mail</FormLabel>
+        <Input
+          focusBorderColor={primaryColor}
+          type="email"
+          placeholder="chatterbox@gmail.com"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup>
+          <Input
+            focusBorderColor={primaryColor}
+            type={show ? "text" : "password"}
+            placeholder="Enter your password..."
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width={"4.5rem"}>
+            <Button h={"1.5rem"} size={"sm"} onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+
+      <Button
+        _hover={{ bg: primaryColor }}
+        width={"100%"}
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+      >
+        Login
+      </Button>
+
+      <Button
+        _hover={{ bg: primaryColor }}
+        width={"100%"}
+        style={{ marginTop: 15 }}
+        onClick={() => {
+          setEmail("guest@example.com");
+          setPassword("123456");
+        }}
+      >
+        Click to Login as Guest
+      </Button>
+    </VStack>
+  );
 };
 
 export default Login;
