d66649e729e2c79ef1a3af6ff234b7d0e42b98a5
nainoktv <nainoaktvillegas@gmail.com>
2023-08-19 00:11:40 -0700

    add all endpoints for chat routes

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -4,3 +4,9 @@ const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+router.route("/groupadd").put(protect, addToGroup);
+
+module.exports = router;
