cb23385c01cc1e44b7a9ac68a062c34f59dbd13b
nainoktv <nainoaktvillegas@gmail.com>
2023-08-24 23:58:21 -0700

    finish rename group route

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -105,5 +105,26 @@ const createGroupChat = asyncHandler(async (req, res) => {
   }
 });
 
-const renameGroup = asyncHandler(async (req, res) => {});
-module.exports = { accessChat, fetchChats, createGroupChat };
+const renameGroup = asyncHandler(async (req, res) => {
+  const { chatId, chatName } = req.body;
+
+  const updatedChat = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      chatName,
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "password");
+
+  if (!updatedChat) {
+    res.status(404);
+    throw new Error("Chat not found");
+  } else {
+    res.json(updatedChat);
+  }
+});
+module.exports = { accessChat, fetchChats, createGroupChat, renameGroup };
