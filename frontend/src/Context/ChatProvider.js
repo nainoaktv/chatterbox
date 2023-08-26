7b10f4788794b948fed638323560f0cde2274a58
nainoktv <nainoaktvillegas@gmail.com>
2023-08-26 00:02:48 -0700

    useHistory deprecated and replaced with useNavigate

--- frontend/src/Context/ChatProvider.js
+++ frontend/src/Context/ChatProvider.js
@@ -1,9 +1,31 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ChatContext = createContext();
 
 const ChatProvider = ({ children }) => {
-  return <ChatContext.Provider>{children}</ChatContext.Provider>;
+  const [user, setUser] = useState();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+
+    if (!userInfo) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  return (
+    <ChatContext.Provider value={{ user, setUser }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  return useContext(ChatContext);
 };
 
 export default ChatProvider;
