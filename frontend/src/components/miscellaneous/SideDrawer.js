374ef6c031e3a4859017b18aedb8e805f761f03d
nainoktv <nainoaktvillegas@gmail.com>
2023-08-26 18:08:05 -0700

    start button to search users through side drawer component

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@chakra-ui/layout";
+import { Tooltip } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/button";
 
 const SideDrawer = () => {
-  return <div>SideDrawer</div>;
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState();
+
+  return (
+    <>
+      <Box>
+        <Tooltip label="Search Users to chat with" hasArrow placement="bottom">
+          <Button variant={"ghost"}></Button>
+        </Tooltip>
+      </Box>
+    </>
+  );
 };
 
 export default SideDrawer;
