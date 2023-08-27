ac530fd29c7d4d875479f69ea99bca3b2f33ed12
nainoktv <nainoaktvillegas@gmail.com>
2023-08-26 17:52:00 -0700

    removing deprecated chakra ui properties for proper spacing

--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Box } from "@chakra-ui/layout";
-import axios from "axios";
+// import axios from "axios";
 import { ChatState } from "../Context/ChatProvider";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
 import ChatBox from "../components/ChatBox";
@@ -11,7 +11,13 @@ const Chatpage = () => {
   return (
     <div style={{ width: "100%" }}>
       {user && <SideDrawer />}
-      <Box>
+      <Box
+        display={"flex"}
+        justifyContent={"space-between"}
+        w={"100%"}
+        h={"91.5vh"}
+        p={"10px"}
+      >
         {user && <MyChats />}
         {user && <ChatBox />}
       </Box>
