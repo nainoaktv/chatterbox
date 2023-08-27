491678bfe4836ca40fdaad6314a741bfc167f5ba
nainoktv <nainoaktvillegas@gmail.com>
2023-08-27 01:20:11 -0700

    initial commit profile modal

---
+++ frontend/src/components/miscellaneous/ProfileModal.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ProfileModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return <div>ProfileModal</div>;
+};
+
+export default ProfileModal;
