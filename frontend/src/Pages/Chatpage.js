c854545e9eaabb18be36f387bf0c77ead49dec14
nainoktv <nainoaktvillegas@gmail.com>
2023-08-26 17:39:37 -0700

    initial commit for chatbox and mychats components

--- frontend/src/Pages/Chatpage.js
+++ frontend/src/Pages/Chatpage.js
@@ -3,6 +3,8 @@ import { Box } from "@chakra-ui/layout";
 import axios from "axios";
 import { ChatState } from "../Context/ChatProvider";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
+import ChatBox from "../components/ChatBox";
+import MyChats from "../components/MyChats";
 
 const Chatpage = () => {
   const { user } = ChatState();
@@ -10,8 +12,8 @@ const Chatpage = () => {
     <div style={{ width: "100%" }}>
       {user && <SideDrawer />}
       <Box>
-        {/* {user && <MyChats />} */}
-        {/* {user && <ChatBox />}    */}
+        {user && <MyChats />}
+        {user && <ChatBox />}
       </Box>
     </div>
   );
