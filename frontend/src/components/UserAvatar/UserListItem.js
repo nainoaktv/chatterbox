82c83eb7813921533eba4f3ee15e72cef2d41a6f
nainoktv <nainoaktvillegas@gmail.com>
2023-08-31 17:16:14 -0700

    complete feature to search users in side drawer

--- frontend/src/components/UserAvatar/UserListItem.js
+++ frontend/src/components/UserAvatar/UserListItem.js
@@ -1,10 +1,46 @@
 import React from "react";
 import { ChatState } from "../../Context/ChatProvider";
+import { Avatar, Box, Text } from "@chakra-ui/react";
 
-const UserListItem = ({ handleFunction }) => {
-  const { user } = ChatState();
+// color palette
+const primaryColor = "#0B0C10";
+const secondaryColor = "#1F2833";
+const greyColor = "#C5C6C7";
+const blueOne = "#66FCF1";
+const blueTwo = "#45A29E";
 
-  return <div></div>;
+const UserListItem = ({ user, handleFunction }) => {
+  return (
+    <Box
+      onClick={handleFunction}
+      cursor={"pointer"}
+      bg={primaryColor}
+      _hover={{ background: blueTwo, color: "white" }}
+      w={"100%"}
+      display={"flex"}
+      alignItems={"center"}
+      color={"black"}
+      px={3}
+      py={2}
+      mb={2}
+      borderRadius={"lg"}
+    >
+      <Avatar
+        mr={2}
+        size={"sm"}
+        cursor={"pointer"}
+        name={user.name}
+        src={user.pic}
+      />
+      <Box>
+        <Text color={blueOne}>{user.name}</Text>
+        <Text color={"white"} fontSize={"xs"}>
+          <b>Email: </b>
+          {user.email}
+        </Text>
+      </Box>
+    </Box>
+  );
 };
 
 export default UserListItem;
