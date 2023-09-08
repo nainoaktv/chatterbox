52e587a8a267c399387deafd3e120a56bbf01732
nainoktv <nainoaktvillegas@gmail.com>
2023-09-08 10:23:09 -0700

    initial commit for chat box

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { ChatState } from "../Context/ChatProvider";
+import { Box } from "@chakra-ui/react";
+
+const primaryColor = "#0B0C10";
+const secondaryColor = "#1F2833";
+const greyColor = "#C5C6C7";
+const blueOne = "#66FCF1";
+const blueTwo = "#45A29E";
 
 const ChatBox = () => {
-  return <div>ChatBox</div>;
+  const { selectedChat } = ChatState();
+  return (
+    <Box
+      display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems={"center"}
+      flexDir={"column"}
+      p={3}
+      bg={secondaryColor}
+      w={{ base: "100%", md: "68%" }}
+      borderRadius={"lg"}
+      borderWidth={"1px"}
+      borderColor={blueTwo}
+    >
+      Single Chat
+    </Box>
+  );
 };
 
 export default ChatBox;
