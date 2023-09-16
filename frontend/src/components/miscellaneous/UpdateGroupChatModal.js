1726060d564ddc6175c5473dee907bf4e4f2ea27
nainoktv <nainoaktvillegas@gmail.com>
2023-09-16 16:35:51 -0700

    complete feature to update groupchat name

--- frontend/src/components/miscellaneous/UpdateGroupChatModal.js
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.js
@@ -2,7 +2,9 @@ import { ViewIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
+  FormControl,
   IconButton,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -16,6 +18,14 @@ import {
 import React, { useState } from "react";
 import { ChatState } from "../../Context/ChatProvider";
 import UserBadgeItem from "../UserAvatar/UserBadgeItem";
+import axios from "axios";
+
+// color palette
+const primaryColor = "#0B0C10";
+const secondaryColor = "#1F2833";
+const orangeColor = "#ED980C";
+const blueOne = "#66FCF1";
+const blueTwo = "#45A29E";
 
 const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -31,6 +41,47 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
 
   const handleRemove = () => {};
 
+  const handleRename = async () => {
+    if (!groupChatName) return;
+
+    try {
+      setRenameLoading(true);
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.put(
+        "/api/chat/rename",
+        {
+          chatId: selectedChat._id,
+          chatName: groupChatName,
+        },
+        config
+      );
+
+      setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setRenameLoading(false);
+    } catch (err) {
+      toast({
+        title: "Error Occured!",
+        description: err.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setRenameLoading(false);
+    }
+
+    setGroupChatName("");
+  };
+
+  const handleSearch = () => {};
+
   return (
     <>
       <IconButton
@@ -41,7 +92,7 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
 
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
-        <ModalContent>
+        <ModalContent bg={secondaryColor} color={"white"}>
           <ModalHeader
             fontSize={"35px"}
             fontFamily={"Montserrat"}
@@ -61,10 +112,36 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
                 />
               ))}
             </Box>
+            <FormControl display={"flex"}>
+              <Input
+                placeholder="Chat Name"
+                focusBorderColor={blueTwo}
+                mb={3}
+                value={groupChatName}
+                onChange={(e) => setGroupChatName(e.target.value)}
+              />
+              <Button
+                variant={"solid"}
+                ml={1}
+                _hover={{ bg: blueOne }}
+                isLoading={renameLoading}
+                onClick={handleRename}
+              >
+                Update
+              </Button>
+            </FormControl>
+            <FormControl>
+              <Input
+                placeholder="Add User to Group"
+                focusBorderColor={blueTwo}
+                mb={1}
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </FormControl>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            <Button _hover={{ bg: blueOne }} mr={3} onClick={onClose}>
               Close
             </Button>
           </ModalFooter>
