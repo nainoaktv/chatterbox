39d003db5bb2b2424914f0784c6e596236db4021
nainoktv <nainoaktvillegas@gmail.com>
2023-09-16 14:35:31 -0700

    add chat name and user badge items to group chat modal

--- frontend/src/components/miscellaneous/UpdateGroupChatModal.js
+++ frontend/src/components/miscellaneous/UpdateGroupChatModal.js
@@ -1,5 +1,6 @@
 import { ViewIcon } from "@chakra-ui/icons";
 import {
+  Box,
   Button,
   IconButton,
   Modal,
@@ -10,11 +11,26 @@ import {
   ModalHeader,
   ModalOverlay,
   useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+import { ChatState } from "../../Context/ChatProvider";
+import UserBadgeItem from "../UserAvatar/UserBadgeItem";
 
 const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [groupChatName, setGroupChatName] = useState();
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [renameLoading, setRenameLoading] = useState(false);
+
+  const toast = useToast();
+
+  const { selectedChat, setSelectedChat, user } = ChatState();
+
+  const handleRemove = () => {};
+
   return (
     <>
       <IconButton
@@ -23,12 +39,29 @@ const UpdateGroupChatModal = ({ fetchAgain, setFetchAgain }) => {
         onClick={onOpen}
       />
 
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader
+            fontSize={"35px"}
+            fontFamily={"Montserrat"}
+            display={"flex"}
+            justifyContent={"center"}
+          >
+            {selectedChat.chatName}
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody></ModalBody>
+          <ModalBody>
+            <Box w={"100%"} display={"flex"} flexWrap={"wrap"} pb={3}>
+              {selectedChat.users.map((u) => (
+                <UserBadgeItem
+                  key={user._id}
+                  user={u}
+                  handleFunction={() => handleRemove(u)}
+                />
+              ))}
+            </Box>
+          </ModalBody>
 
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={onClose}>
