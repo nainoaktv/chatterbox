730e1d73be2e25355b0635de938c7eb859d83272
nainoktv <nainoaktvillegas@gmail.com>
2023-09-18 16:22:00 -0700

    fixed issue where logged in users avatar would appear in chat

--- frontend/src/components/ScrollableChat.js
+++ frontend/src/components/ScrollableChat.js
@@ -1,12 +1,44 @@
 import React from "react";
 import ScrollableFeed from "react-scrollable-feed";
+import { isSameSender, isLastMessage } from "../config/ChatLogics";
+import { ChatState } from "../Context/ChatProvider";
+import { Avatar, Tooltip } from "@chakra-ui/react";
 
 const ScrollableChat = ({ messages }) => {
+  const { user } = ChatState();
   return (
     <ScrollableFeed>
       {messages &&
         messages.map((m, i) => (
-          <div style={{ display: "flex" }} key={m._id}></div>
+          <div style={{ display: "flex" }} key={m._id}>
+            {(isSameSender(messages, m, i, user._id) ||
+              isLastMessage(messages, i, user._id)) && (
+              <Tooltip label={m.sender.name} placement="bottom-start" hasArrow>
+                <Avatar
+                  mt={"7px"}
+                  mr={1}
+                  size={"sm"}
+                  cursor={"pointer"}
+                  name={m.sender.name}
+                  src={m.sender.pic}
+                />
+              </Tooltip>
+            )}
+
+            <span
+              style={{
+                backgroundColor: `${
+                  m.sender._id === user._id ? "#ED980C" : "#45A29E"
+                }`,
+                borderRadius: "20px",
+                padding: "5px 15px",
+                maxWidth: "75%",
+                color: "black",
+              }}
+            >
+              {m.content}
+            </span>
+          </div>
         ))}
     </ScrollableFeed>
   );
