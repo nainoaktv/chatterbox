4f0c88721f7397be042139538a627b5ebce5a46b
nainoktv <nainoaktvillegas@gmail.com>
2023-09-18 20:09:07 -0700

    add socket for typing indicator

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,9 @@ io.on("connection", (socket) => {
     console.log(`User joined room: ${room}`);
   });
 
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
   socket.on("new message", (newMessageReceived) => {
     var chat = newMessageReceived.chat;
 
