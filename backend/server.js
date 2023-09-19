d11f170c1c8c19eef7b89d4d8a456eefffb512c7
nainoktv <nainoaktvillegas@gmail.com>
2023-09-18 21:23:23 -0700

    turn off socket when user is no longer connected to app

--- backend/server.js
+++ backend/server.js
@@ -66,4 +66,9 @@ io.on("connection", (socket) => {
       socket.in(user._id).emit("message received", newMessageReceived);
     });
   });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
 });
